18fc0ef69e7f9842ed91d1405f6958b344102023
Darren Ferguson <df@darren-ferguson.com>
2016-11-17 11:23:10 +0000

    Add Umbraco Helper.

---
+++ Moriyama.AzureSearch.Umbraco.Application/Umbraco/UmbracoHelperExetnsion.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using Umbraco.Web;
+
+namespace Moriyama.AzureSearch.Umbraco.Application.Umbraco
+{
+    public static class UmbracoHelperExetnsion
+    {
+        public static AzureSearchClient SearchContent(this UmbracoHelper helper)
+        {
+            return new AzureSearchClient(HttpContext.Current.Server.MapPath("/"));
+        }
+    }
+}
