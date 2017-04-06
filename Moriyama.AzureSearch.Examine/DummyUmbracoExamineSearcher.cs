65804d507df6ee5890ed70d1ba649db18b17a1cd
Darren Ferguson <df@darren-ferguson.com>
2017-04-06 15:18:43 +0100

    Dummy Examine searcher - needed by typedmedia fallback.

---
+++ Moriyama.AzureSearch.Examine/DummyUmbracoExamineSearcher.cs
@@ -0,0 +1,17 @@
+﻿using Examine;
+using Examine.SearchCriteria;
+using System;
+using System.IO;
+using UmbracoExamine;
+
+namespace Moriyama.AzureSearch.Examine
+{
+    public class DummyUmbracoExamineSearcher : UmbracoExamineSearcher
+    {
+        public override ISearchResults Search(ISearchCriteria searchParams)
+        {
+            throw new FileNotFoundException("");
+            
+        }
+    }
+}
