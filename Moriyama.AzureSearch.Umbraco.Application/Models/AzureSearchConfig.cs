4ff136d6256b4126c0e93f10c3bdf2424e62fe50
Tom Pipe <tompipe2008@gmail.com>
2017-12-20 14:14:44 +0000

    Add support for creating analyzers and scoring profiles from json configuration

--- Moriyama.AzureSearch.Umbraco.Application/Models/AzureSearchConfig.cs
+++ Moriyama.AzureSearch.Umbraco.Application/Models/AzureSearchConfig.cs
@@ -1,4 +1,8 @@
-﻿namespace Moriyama.AzureSearch.Umbraco.Application.Models
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Search.Models;
+
+namespace Moriyama.AzureSearch.Umbraco.Application.Models
 {
     public class AzureSearchConfig
     {
@@ -9,5 +13,9 @@
         public string IndexName { get; set; }
 
         public SearchField[] SearchFields { get; set; }
+
+        public Analyzer[] Analyzers {get; set;} 
+
+        public ScoringProfile[] ScoringProfiles {get; set;}
     }
 }
