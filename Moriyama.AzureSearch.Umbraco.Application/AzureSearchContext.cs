5d98a6f03698c0e8ed1155da3373d55557d3d1cb
Emiliano Fabioli <emiliano@moriyama.co.uk>
2018-01-08 16:08:50 +0100

    made sure that  AzureSearchContext class is instantiated correctly

--- Moriyama.AzureSearch.Umbraco.Application/AzureSearchContext.cs
+++ Moriyama.AzureSearch.Umbraco.Application/AzureSearchContext.cs
@@ -1,15 +1,22 @@
-﻿using Moriyama.AzureSearch.Umbraco.Application.Interfaces;
+﻿using log4net;
+using Moriyama.AzureSearch.Umbraco.Application.Interfaces;
 using System;
 using System.Configuration;
+using System.Reflection;
 using System.Web;
 
 namespace Moriyama.AzureSearch.Umbraco.Application
 {
     public class AzureSearchContext
     {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         private static AzureSearchContext _instance;
+
         private IAzureSearchIndexClient _searchIndexClient { get; set; }
+
         private IAzureSearchClient _searchClient { get; set; }
+
         private AzureSearchContext() { }
 
         public static AzureSearchContext Instance
@@ -18,12 +25,22 @@ namespace Moriyama.AzureSearch.Umbraco.Application
             {
                 if (_instance == null)
                 {
-                    _instance = new AzureSearchContext();
-
-                    if (_instance.AutomaticStartUp)
+                    try
                     {
-                        var appRoot = ConfigurationManager.AppSettings["Moriyama.AzureSearch.AppRoot"] ?? "/";
-                        _instance.StartUp(HttpContext.Current.Server.MapPath(appRoot));
+                        _instance = new AzureSearchContext();
+
+                        if (_instance.AutomaticStartUp)
+                        {
+                            var appRoot = ConfigurationManager.AppSettings["Moriyama.AzureSearch.AppRoot"] ?? "/";
+                            _instance.StartUp(HttpContext.Current.Server.MapPath(appRoot));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // generic catch is not a great deal but we need to make sure
+                        // nothing goes wrong otherwise AzureSearchContext will be never consolidated.
+                        _instance = null;
+                        Log.Error("AzureSearchContext cannot be instantiated.", ex);
                     }
                 }
                 return _instance;
@@ -60,6 +77,7 @@ namespace Moriyama.AzureSearch.Umbraco.Application
         {
             _searchClient = new AzureSearchClient(appRoot);
             _searchIndexClient = new AzureSearchIndexClient(appRoot);
+            Log.Info("AzureSearchContext instantiated correctly.");
         }
 
         public bool AutomaticStartUp
