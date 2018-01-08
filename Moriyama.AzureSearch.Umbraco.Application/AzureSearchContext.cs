0db086b1d8c86ade966fb4fe0e94887f568f36f3
Emiliano Fabioli <emiliano@moriyama.co.uk>
2018-01-08 15:26:35 +0100

    made azure search start up more flexible

--- Moriyama.AzureSearch.Umbraco.Application/AzureSearchContext.cs
+++ Moriyama.AzureSearch.Umbraco.Application/AzureSearchContext.cs
@@ -1,11 +1,15 @@
 ﻿using Moriyama.AzureSearch.Umbraco.Application.Interfaces;
+using System;
+using System.Configuration;
+using System.Web;
 
 namespace Moriyama.AzureSearch.Umbraco.Application
 {
     public class AzureSearchContext
     {
         private static AzureSearchContext _instance;
-
+        private IAzureSearchIndexClient _searchIndexClient { get; set; }
+        private IAzureSearchClient _searchClient { get; set; }
         private AzureSearchContext() { }
 
         public static AzureSearchContext Instance
@@ -15,13 +19,56 @@ namespace Moriyama.AzureSearch.Umbraco.Application
                 if (_instance == null)
                 {
                     _instance = new AzureSearchContext();
+
+                    if (_instance.AutomaticStartUp)
+                    {
+                        var appRoot = ConfigurationManager.AppSettings["Moriyama.AzureSearch.AppRoot"] ?? "/";
+                        _instance.StartUp(HttpContext.Current.Server.MapPath(appRoot));
+                    }
                 }
                 return _instance;
             }
         }
 
-        public IAzureSearchIndexClient SearchIndexClient { get; internal set; }
+        public IAzureSearchIndexClient SearchIndexClient
+        {
+            get
+            {
+                if (_searchIndexClient == null && !AutomaticStartUp)
+                {
+                    throw new NullReferenceException("Automatic start up is disabled; make sure to call StartUp(appRoot) explicitly.");
+                }
 
-        public IAzureSearchClient SearchClient { get; internal set; }
+                return _searchIndexClient;
+            }
+        }
+
+        public IAzureSearchClient SearchClient
+        {
+            get
+            {
+                if (_searchClient == null && !AutomaticStartUp)
+                {
+                    throw new NullReferenceException("Automatic start up is disabled; make sure to call StartUp(appRoot) explicitly.");
+                }
+
+                return _searchClient;
+            }
+        }
+
+        public void StartUp(string appRoot)
+        {
+            _searchClient = new AzureSearchClient(appRoot);
+            _searchIndexClient = new AzureSearchIndexClient(appRoot);
+        }
+
+        public bool AutomaticStartUp
+        {
+            get
+            {
+                var disabledStartUp = ConfigurationManager.AppSettings["Moriyama.AzureSearch.DisabledStartUp"] ?? "False";
+                return !bool.Parse(disabledStartUp);
+            }
+        }
     }
 }
