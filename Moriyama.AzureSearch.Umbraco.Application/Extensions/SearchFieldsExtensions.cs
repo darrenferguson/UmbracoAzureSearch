cfcac84d0955d01a8c34b1811e2435426a6af9a2
Tom Pipe <tompipe2008@gmail.com>
2018-01-10 00:59:05 +0000

    start to pull back in examine stuff from tom

--- Moriyama.AzureSearch.Umbraco.Application/Extensions/SearchFieldsExtensions.cs
+++ Moriyama.AzureSearch.Umbraco.Application/Extensions/SearchFieldsExtensions.cs
@@ -1,11 +1,47 @@
 ﻿using Microsoft.Azure.Search.Models;
 using Moriyama.AzureSearch.Umbraco.Application.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Examine.LuceneEngine.Config;
+using Microsoft.Azure.Search.Models;
+
 using Moriyama.AzureSearch.Umbraco.Application.Models;
+using Umbraco.Core;
 
 namespace Moriyama.AzureSearch.Umbraco.Application.Extensions
 {
     public static class SearchFieldsExtensions
     {
+        //public static void CopyFieldsFromExamineIndexSet(string examineSetName)
+        //{
+        //    var indexSet = IndexSets.Instance.Sets[examineSetName];
+        //    if (indexSet != null)
+        //    {
+        //        var searchClient = AzureSearchContext.Instance.SearchIndexClient;
+        //        var config = searchClient.GetConfiguration();
+        //        var configuredFields = config.Fields?.ToDictionary(f => f.Name);
+
+        //        foreach (IndexField userField in indexSet.IndexUserFields)
+        //        {
+        //            SearchField field;
+        //            if (!configuredFields.TryGetValue(userField.Name, out field))
+        //            {
+        //                field = new SearchField();
+        //                configuredFields.Add(userField.Name, field);
+        //            }
+
+        //            field.Name = userField.Name;
+        //            //field.FieldType = userField.Type; // TODO: test type mapping
+        //            field.IsSortable = userField.EnableSorting;
+        //            field.IsSearchable = true;
+        //        }
+
+
+        //        config.Fields = configuredFields.Select(f => f.Value).ToArray();
+        //    }
+        //}
+
         public static Field ToAzureField(this SearchField field)
         {
             var t = DataType.String;
