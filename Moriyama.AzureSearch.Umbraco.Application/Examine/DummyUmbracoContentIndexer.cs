d49536085c9a29f45e169b15f9bb0b3d9dd92886
Tom Pipe <tompipe2008@gmail.com>
2018-01-26 11:21:47 +0000

    Return correct fields from azure to the examine dashboard

--- Moriyama.AzureSearch.Umbraco.Application/Examine/DummyUmbracoContentIndexer.cs
+++ Moriyama.AzureSearch.Umbraco.Application/Examine/DummyUmbracoContentIndexer.cs
@@ -30,5 +30,30 @@ namespace Moriyama.AzureSearch.Umbraco.Application.Examine
 
         }
 
+        protected override IIndexCriteria GetIndexerData(IndexSet indexSet)
+        {
+            // examine fields
+            var data = base.GetIndexerData(indexSet);
+
+            // azure config
+            var path = System.Web.Hosting.HostingEnvironment.MapPath("~/");
+            var serviceClient = new AzureSearchIndexClient(path);
+            
+            var examineStandardFields = data.StandardFields;
+            var examineUserFields = data.UserFields;
+
+            var azureStandardFields = serviceClient.GetStandardUmbracoFields().ToDictionary(f => f.Name);
+            var azureUserFields = serviceClient.GetConfiguration().SearchFields.ToDictionary(f => f.Name);
+            
+            var standardToRemove = examineStandardFields.Where(examineField => !azureStandardFields.Keys.InvariantContains(examineField.Name));
+            var validStandardFields = examineStandardFields.Except(standardToRemove);
+
+            var userToRemove = examineUserFields.Where(examineField => !azureUserFields.Keys.InvariantContains(examineField.Name));
+            var validUserFields = examineUserFields.Except(userToRemove);
+
+            var newData = new IndexCriteria(validStandardFields, validUserFields, data.IncludeNodeTypes, data.ExcludeNodeTypes, data.ParentNodeId);
+
+            return newData;
+        }
     }
 }
