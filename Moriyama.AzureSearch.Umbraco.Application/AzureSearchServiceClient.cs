89a61c33752db7f7e3a2218449651120cee74eb3
Lee Cook <leethomascook@hotmail.co.uk>
2019-02-21 21:51:20 +0000

    Whitespace formatting

--- Moriyama.AzureSearch.Umbraco.Application/AzureSearchServiceClient.cs
+++ Moriyama.AzureSearch.Umbraco.Application/AzureSearchServiceClient.cs
@@ -100,7 +100,7 @@ namespace Moriyama.AzureSearch.Umbraco.Application
                     from cmsContent, umbracoNode where
                     cmsContent.nodeId = umbracoNode.id and
                     umbracoNode.nodeObjectType = 'C66BA18E-EAF3-4CFF-8A22-41B16D66A972'
- and umbracoNode.[trashed] = 0");
+ 		    and umbracoNode.[trashed] = 0");
 
                 mediaIds = db.Fetch<int>(@"select distinct cmsContent.NodeId
                     from cmsContent, umbracoNode where
