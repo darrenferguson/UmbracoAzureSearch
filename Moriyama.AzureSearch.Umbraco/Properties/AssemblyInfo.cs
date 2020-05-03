d64be4a42ad38e6ffb98ab157846360b9b4a1b1f
Darren Ferguson <df@darren-ferguson.com>
2020-05-03 11:25:09 +0100

    Set up project structure for Umbraco 8 version.

--- Moriyama.AzureSearch.Umbraco/Properties/AssemblyInfo.cs
+++ Moriyama.AzureSearch.Umbraco/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Moriyama.AzureSearch.Umbraco")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d341b68-8c61-411b-9475-0e87f5a9d120")]
+[assembly: Guid("53ed5cc3-ca45-4bfa-9764-7cf87e6180ae")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.8.1")]
-[assembly: AssemblyFileVersion("1.3.8.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
